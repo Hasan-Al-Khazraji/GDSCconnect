129beda8d5c26e5287669337faa9de19d80d060c
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2025-04-27 21:37:57 -0400

    Add react routes for each id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/:id" element={<Home/>} />
+    </Routes>
   );
 }
 
