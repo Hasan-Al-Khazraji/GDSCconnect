5e680f9bc097ec59ffb4ed257e413786aa1feee3
timothytenzin20 <timothytenzin2004@gmail.com>
2025-05-01 01:23:23 -0400

    edit privileges

--- client/src/components/ProfileContainer.tsx
+++ client/src/components/ProfileContainer.tsx
@@ -1,15 +1,39 @@
 import ProfileHero from "./ProfileHero";
+import { useAuth } from "../Contexts/AuthContext";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useParams } from "react-router-dom"; 
 
 export default function ProfileContainer() {
+    const { authUser } = useAuth();
+    const { id } = useParams();
+    const [canEdit, setCanEdit] = useState(false);
 
-    function colorLetters(str: string) {
-        const colors = ['text-[#4285F4]', 'text-[#EA4335]', 'text-[#FBBC05]', 'text-[#34A853]'];
-        let coloredStr = '';
-        for (let i = 0; i < str.length; i++) {
-            coloredStr += `<span class="${colors[i % colors.length]}">${str[i]}</span>`;
-        }
-        return coloredStr;
-    }
+    useEffect(() => {
+        const checkEmailMatch = async () => {
+            if (authUser?.email && id) { 
+                try {
+                    const docRef = doc(db, "hackers", id); 
+                    const snapshot = await getDoc(docRef);
+
+                    if (snapshot.exists()) {
+                        const data = snapshot.data();
+                        if (data.email === authUser.email) {
+                            setCanEdit(true);
+                        } else {
+                            setCanEdit(false);
+                            console.log("Email does not match the one in the database.");
+                        }
+                    }
+                } catch (error) {
+                    console.error("Error fetching profile data:", error);
+                }
+            }
+        };
+
+        checkEmailMatch();
+    }, [authUser, id]); 
 
     return (
         <div className="border-4 border-black rounded-3xl mt-8">
@@ -18,7 +42,14 @@ export default function ProfileContainer() {
             </div>
             <div className="bg-white rounded-b-2xl pt-8">
                 <ProfileHero />
-                <img src={require("../assets/images/GDG_Watermark.png")} alt="Hello" className="w-24 p-3" />
+                <div className="flex items-center justify-between">
+                    <img src={require("../assets/images/GDG_Watermark.png")} alt="Hello" className="w-24 p-3" />
+                    {canEdit && (
+                        <button className="bg-black text-white font-bold py-2 px-4 rounded-full ml-auto mr-4 hover:bg-gray-800 transition duration-300 ease-in-out">
+                            Edit Profile
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
     );
