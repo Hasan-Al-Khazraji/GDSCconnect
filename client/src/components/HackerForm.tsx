77048d97ec106a2bd27d52d8b6ce9d81714262c7
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2025-05-02 03:05:41 -0400

    Upgrade landing page

--- client/src/components/HackerForm.tsx
+++ client/src/components/HackerForm.tsx
@@ -8,6 +8,7 @@ interface HackerFormProps {
 
 export default function HackerForm({ onRedirect }: HackerFormProps) {
   const [uniqueId, setUniqueId] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -25,14 +26,14 @@ export default function HackerForm({ onRedirect }: HackerFormProps) {
             onRedirect(`/${uniqueId}`);
           }
         } else {
-          alert('INVALID ID: Enter a valid unique ID from 001 to 250.');
+          setError('INVALID ID: Enter a valid unique ID from 001 to 250.');
         }
       } catch (error) {
         console.error('Error fetching data:', error);
-        alert('An error occurred while fetching data.');
+        setError('An error occurred while fetching data.');
       }
     } else {
-      alert('Please enter a valid unique ID from 001 to 250.');
+      setError('Please enter a valid unique ID from 001 to 250.');
     }
   };
 
@@ -42,9 +43,10 @@ export default function HackerForm({ onRedirect }: HackerFormProps) {
         type="text"
         value={uniqueId}
         onChange={(e) => setUniqueId(e.target.value)}
-        placeholder="Enter unique integer ID"
+        placeholder="Enter unique integer ID..."
         className="p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
+      {error && <p className="text-red-500 text-center mb-4">{error}</p>}
       <button
         type="submit"
         className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,4 +1,5 @@
 import HackerForm from '../components/HackerForm';
+import ResponsiveWrapper from '../components/ResponsiveWrapper';
 
 export default function Landing() {
   const handleRedirect = (url: string) => {
@@ -6,9 +7,12 @@ export default function Landing() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Scan QR Code or Enter A Hacker ID</h1>
-      <HackerForm onRedirect={handleRedirect} />
-    </div>
+    <ResponsiveWrapper>
+      <div className="flex flex-col items-center justify-center h-screen font-GoogleSansDisplay">
+        <img src={require("../assets/images/GDSC_Hacks_Logo.png")} alt="Hello" className="max-w-full" />
+        <h1 className="text-2xl font-bold mb-4">Scan QR Code or Enter A Hacker ID</h1>
+        <HackerForm onRedirect={handleRedirect} />
+      </div>
+    </ResponsiveWrapper>
   );
 }
